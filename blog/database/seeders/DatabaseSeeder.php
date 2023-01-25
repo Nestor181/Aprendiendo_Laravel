d77af7840b0f5971b61549385f9f83c9d733d5d9
E_n_i <nestorfm801@gmail.com>
2023-01-25 12:45:33 -0600

    Crear un seeder para hacer inserciones de ejemplo

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\curso;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+       // $this->call(CursoSeeder::class); //Para utilizar todo lo escrito en el método Run del archivo CursoSeeder
+        Curso::factory(50)->create();//Llamamos al archivo curso para crear 50 datos de prueba.
+        User::factory(10)->create();
+       
     }
 }
