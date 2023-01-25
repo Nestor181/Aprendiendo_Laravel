8ffcd58fe91448d0d914eb38ed36a6fdf8c94e29
E_n_i <nestorfm801@gmail.com>
2023-01-25 12:43:43 -0600

    Crear datos de insercion como ejemplo para la bd.

---
+++ blog/database/factories/CursoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\curso;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Curso>
+ */
+class CursoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        //En este apartado se llenan cada una de los indices con datos de prueba para hacer mas inserciones de ejemplo.
+        return [
+            'name' => $this->faker->sentence(),
+            'descripcion' => $this->faker->paragraph(), 
+            'categoria' => $this->faker->randomElement(['Desarrollo web','Diseño web'])
+            
+        ];
+    }
+}
