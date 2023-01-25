3a246fcbe5ce369c23bef992a846a7e7df069527
E_n_i <nestorfm801@gmail.com>
2023-01-25 12:44:49 -0600

    Agregue un indice para categoria en usuario.

--- blog/database/migrations/2023_01_24_182901_create_cursos_table.php
+++ blog/database/migrations/2023_01_24_182901_create_cursos_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('descripcion');    
+            $table->text('descripcion');
+            $table->text('categoria');
             $table->timestamps();
         });
     }
