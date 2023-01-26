6c2c3421a4e0dad9be52f845bcafcf13e5c44a40
Jonathan Calzada <jcalzadamtz@gmail.com>
2023-01-26 13:01:02 -0600

    Añadir boton para la opcion de eliminar registro

--- blog/resources/views/cursos/show.blade.php
+++ blog/resources/views/cursos/show.blade.php
@@ -6,6 +6,13 @@
     <a href="{{toute('cursos.edit', $curso)}}"> Editar curso </a>
     <p><strong> Categoria: </strong> {{$curso->categoria}}</p> <!--Para mostrar la categoria del curso donde se encuentra-->
     <p>{{$curso->descripcion}}</p> <!--Muestra la descripcion del curso donde se encuenttra-->
+
+    <!--Crear un boton para dar la opcion de eliminar un registro de la bd-->
+    <form action="{{route('cursos.destroy', $curso)}}" method="post">
+        @csrf
+        @method('delete')
+        <button type="submit">Eliminar</button>
+    </form>
 @section('content')
     
 @endsection
