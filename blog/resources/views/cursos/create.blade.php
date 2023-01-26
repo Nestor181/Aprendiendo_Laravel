7a488b9a64ea4dd9723886b428e41b347c685d56
Jonathan Calzada <jcalzadamtz@gmail.com>
2023-01-26 12:07:23 -0600

    Agregar un formulario a create

--- blog/resources/views/cursos/create.blade.php
+++ blog/resources/views/cursos/create.blade.php
@@ -2,6 +2,50 @@
 
 @section('title', 'cursos create')
     <H1>En esta página podrás crear un curso</H1>
+    <form action="{{route('cursos.store')}}" method='POST'>  <!--Enviar la informacion al link de cursos.store con el metodo post-->
+        @csrf   <!--Con esta directiva de blade agrega un token al momento de enviar el formulario-->
+        <label>
+            Nombre:
+            <br>
+            <input type='text' name='name' value="{{old('name')}}">
+        </label>
+
+        <!--En caso de que se encuentre con un error al momento de llenar el formulario en el apartado name, arrojara un mensaje de error-->
+        @error('name')
+            <br>
+            <small>*{{$message}}</small>
+            <br>
+        @enderror
+
+        <br>
+        <label>
+            Descripcion:
+            <br>
+            <textarea name='descripcion' rows='S'>{{old(descripcion)}}</textarea>
+        </label>
+
+        @error('descripcion')
+            <br>
+            <small>*{{$message}}</small>
+            <br>
+        @enderror
+
+        <br>
+        <label>
+            Categoria:
+            <br>
+            <input name='text' name='categoria' value="{{old('categoria')}}">
+        </label>
+
+        @error('categoira')
+            <br>
+            <small>*{{$message}}</small>
+            <br>
+        @enderror
+
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
 @section('content')
     
 @endsection
