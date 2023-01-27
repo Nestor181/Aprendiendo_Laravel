72eb378749648c3533ebda7e9b9ef9915bfad810
E_n_i <nestorfm801@gmail.com>
2023-01-27 13:09:38 -0600

    Seprando el nombre del curso por guiones(V.23)

--- blog/database/factories/CursoFactory.php
+++ blog/database/factories/CursoFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\curso;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Curso>
@@ -18,9 +19,11 @@ class CursoFactory extends Factory
     protected $model = Curso::class;
     public function definition()
     {
+        $name = $this->faker->sentence();
         //En este apartado se llenan cada una de los indices con datos de prueba para hacer mas inserciones de ejemplo.
         return [
-            'name' => $this->faker->sentence(),
+            'name' => $name,
+            'slug' => Str::slug($name,'_'), //Hace que el nombre que se le pase lo pase a minusculas y los espacios los separe con un guion.(Video 23)
             'descripcion' => $this->faker->paragraph(), 
             'categoria' => $this->faker->randomElement(['Desarrollo web','Diseño web'])
             
