add35d481dfc61b6dce96d9d5f120040c68b8cd9
E_n_i <nestorfm801@gmail.com>
2023-01-27 12:30:19 -0600

    Agregando más links de redireccion.

--- blog/resources/views/cursos/show.blade.php
+++ blog/resources/views/cursos/show.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.plantilla')
 
 @section('title', 'curso '.$curso->name)
-    <H1>Bienvenido al curso {{$curso->name}}</H1>
-    <a href="{{route('cursos.index')}}"> Volver a cursos </a> <!--Para poder regresar al link principal de bienvenido a curso-->
-    <a href="{{route('cursos.edit', $curso)}}"> Editar curso </a>
-    <p><strong> Categoria: </strong> {{$curso->categoria}}</p> <!--Para mostrar la categoria del curso donde se encuentra-->
-    <p>{{$curso->descripcion}}</p> <!--Muestra la descripcion del curso donde se encuenttra-->
-
-    <!--Crear un boton para dar la opcion de eliminar un registro de la bd-->
-    <form action="{{route('cursos.destroy', $curso)}}" method="post">
-        @csrf
-        @method('delete')
-        <button type="submit">Eliminar</button>
-    </form>
+    
 @section('content')
+    <H1>Bienvenido al curso {{$curso->name}}</H1>
+        <a href="{{route('cursos.index')}}"> Volver a página principal </a><!--Para poder regresar al link principal de bienvenido a curso-->
+        <br>
+        <a href="{{route('cursos.edit', $curso)}}"> Editar curso </a>
+        <p><strong> Categoria: </strong>{{$curso->categoria}}</p> <!--Para mostrar la categoria del curso donde se encuentra-->
+        <p><strong>Descripcion: </strong>{{$curso->descripcion}}</p> <!--Muestra la descripcion del curso donde se encuenttra-->
+
+        <!--Crear un boton para dar la opcion de eliminar un registro de la bd-->
+        <form action="{{route('cursos.destroy', $curso)}}" method="post">
+            @csrf
+            @method('delete')
+            <button type="submit">Eliminar</button>
+        </form>
     
 @endsection
 
