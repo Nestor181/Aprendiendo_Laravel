45daa3c260a6520770ddeb0ba5a4a7ec2567e369
E_n_i <nestorfm801@gmail.com>
2023-01-27 11:12:54 -0600

    Corrigiendo errores de mostrar curso.

--- blog/resources/views/cursos/show.blade.php
+++ blog/resources/views/cursos/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'curso '.$curso->name)
     <H1>Bienvenido al curso {{$curso->name}}</H1>
     <a href="{{route('cursos.index')}}"> Volver a cursos </a> <!--Para poder regresar al link principal de bienvenido a curso-->
-    <a href="{{toute('cursos.edit', $curso)}}"> Editar curso </a>
+    <a href="{{route('cursos.edit', $curso)}}"> Editar curso </a>
     <p><strong> Categoria: </strong> {{$curso->categoria}}</p> <!--Para mostrar la categoria del curso donde se encuentra-->
     <p>{{$curso->descripcion}}</p> <!--Muestra la descripcion del curso donde se encuenttra-->
 
