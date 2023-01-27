dd57b56e438591234bd51592d52fc76e34acdb83
E_n_i <nestorfm801@gmail.com>
2023-01-27 11:14:08 -0600

    Agregando la ruta de Models\curso.

--- blog/app/Http/Controllers/CursoController.php
+++ blog/app/Http/Controllers/CursoController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Request\StoreCurso; //Se agrega que se quire utilizar el store curso para las validaciones.
+use App\Http\Requests\StoreCurso;
+//Se agrega que se quire utilizar el store curso para las validaciones.
 
+use App\Models\curso;
 class CursoController extends Controller
 {
     public function index(){//Este método muestra la página principal, tiene este nombre por convención
        // $curso = curso::all();  //Se agrega esta linea para extraer todos los datos de la tabal curso.
-       $curso = Curso::orderBy('id', 'desc')->paginate(); //De esta forma obtenemos los registros de una forma paginada para así mostrar los datos por paginas.
-        return view('cursos.index', compact('curso') ); //Para indicar que esta en una carpeta se utiliza el separador "."
+       $cursos = Curso::orderBy('id', 'desc')->paginate(); //De esta forma obtenemos los registros de una forma paginada para así mostrar los datos por paginas.
+        return view('cursos.index', compact('cursos') ); //Para indicar que esta en una carpeta se utiliza el separador "."
                                                         //Se utiliza compact para enviar la variable al html.
     }
     public function create(){//Este método muestra el formulario para crear cosas, tiene este nombre por convención
@@ -20,15 +22,15 @@ class CursoController extends Controller
     public function store(StoreCurso $request){
 
         //De esta forma ya al obtener toda la informacion del formulario, se agrega un nuevo registro de curso con la informacion del formulario.
-       /* $curso = new Curso();
+        $curso = new Curso();
 
         $curso->name= $request->name;
         $curso->categoria= $request->categoria;
         $curso->descripcion= $request->descripcion;
 
-        $curso->save();*/
+        $curso->save();
 
-        $curso = Curso::create($request->all()); //Se creara un nuevo curso con los datos que coincidan con request.
+        //$curso = Curso::create($request->all()); //Se creara un nuevo curso con los datos que coincidan con request.
 
         return redirect()->route('cursos.show', $curso->id);  //Redirige al link del objeto creado.
 
@@ -54,13 +56,13 @@ class CursoController extends Controller
 
                
         //Actualizamos el curso con la informacion obtenida desde el formulario de update.
-       /* $curso -> name = $request -> name;
+       $curso -> name = $request -> name;
         $curso -> categoria = $request -> categoria;
         $curso -> descripcion = $request -> desripcion;
 
-        $curso -> save();*/
+        $curso -> save();
 
-        $curso->update($request->all()); //Se actualiza todos los registos con la informacion del formulario masivamente.
+        //$curso->update($request->all()); //Se actualiza todos los registos con la informacion del formulario masivamente.
 
 
         return redirect()->route('cursos.show', $curso->id);
