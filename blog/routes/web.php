fd82bb45e58bd166a692b2ee1cf368d9dfcf8eaa
E_n_i <nestorfm801@gmail.com>
2023-01-27 12:31:47 -0600

    Modificando las rutas (V. 22)

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -25,15 +25,16 @@ Route::get('/', HomeController::class);
 
 //Para esta parte se creará un controlador para las tres rutas
 //Primera forma, cada ruta por separado
-Route::get('cursos', [CursoController::class, 'index']);
+/*Route::get('cursos', [CursoController::class, 'index']);
 
 Route::get("cursos/create", [ CursoController::class, 'create' ] );
 
 Route::get('cursos/{curso}', [ CursoController::class, 'show' ] );  //La parte de "{curso}" es donde se envía la url como variable
+*/
 
 
 //Segunda forma, se crea un grupo de rutas que contengan el mismo controlador
-Route::controller(CursoController::class)->group(function(){
+/*Route::controller(CursoController::class)->group(function(){
     Route::get('cursos', 'index') ->name('cursos.index'); //Se agregan los nombres para indentificar las rutas.
 
     Route::get("cursos/create", 'create' ) ->name('cursos.create');
@@ -42,14 +43,16 @@ Route::controller(CursoController::class)->group(function(){
 
     Route::get('cursos/{id}', 'show' )->name('cursos.show');
 
-    Route::get('cursos/{id}/edit', 'edit')->name('cursos.edit');
+    Route::get('cursos/{curso}/edit', 'edit')->name('cursos.edit');
 
     Route::put('cursos/{curso}', 'update')->name('cursos.update'); //Se utiliza el método put porque se recomienda este cuando se quiere actualizar información.
 
-    Route::delete('cursos/{cruso}', 'destroy')->name('cursos.destroy');  //Método para eliminar registros en la base de datos
-});
+    Route::delete('cursos/{curso}', 'destroy')->name('cursos.destroy');  //Método para eliminar registros en la base de datos
+});*/
 
-
+Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas'=>'curso'])->names('cursos');  //Esta línea ahorra el tener que poner tantas url para el mismo controlador. (Video 22)
+                                                                    //El metodo names('NameURL') es para especificar el nombre para nuestras rutas internamente.(Video 22)
+                                                                    //El metodo parameters(['NameURL'=>'NameEnviar']) para indicar como queremos que se llamen las variables a enviar. (Video 22)
 
 //Pasar por la url dos vaiables
 /*Route::get('curso/{curso}/{var2?}', function ($curso,$var2=null) { //El signo "?" en la segunda variable indica que la variable es opcional
