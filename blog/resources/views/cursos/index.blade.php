48d3e0561fe8912d23c049ccafce34d9b09424ef
E_n_i <nestorfm801@gmail.com>
2023-01-31 10:03:47 -0600

    Comentando linea de código.

--- blog/resources/views/cursos/index.blade.php
+++ blog/resources/views/cursos/index.blade.php
@@ -3,14 +3,14 @@
 @section('title', 'cursos')
     
 @section('content')
-    <h1>Bienvenido a la página cursos</h1>
+    <h1><strong>Bienvenido a la página cursos </strong></h1>
     <a href="{{route('cursos.create')}}">Crear curso</a> <!--Con esta linea se hace referencia a la ruta a la que se quire redirigir con el nombre que se le asigno-->
     <ul>
         @foreach($cursos as $curso) 
             <li>
                 <a href="{{route('cursos.show', $curso)}}">{{$curso->name}}</a>
                 <br>
-                {{route('cursos.show', $curso)}}
+               <!--{{route('cursos.show', $curso)}}-->
             </li>
 
         @endforeach
