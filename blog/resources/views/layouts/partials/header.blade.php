38452f10c4508ab18dbe0ba24b4c6f2db89b9825
E_n_i <nestorfm801@gmail.com>
2023-01-31 13:10:19 -0600

    Creando link para mensajeCorreo (V.26)

--- blog/resources/views/layouts/partials/header.blade.php
+++ blog/resources/views/layouts/partials/header.blade.php
@@ -6,6 +6,9 @@
             <li> <a href="{{route('Home')}}" class="{{ request()->routeIs('Home') ? 'active' : ''}}">Home</a> </li>   
             <li> <a href="{{route('cursos.index')}}" class="{{ request()->routeIs('cursos.*') ? 'active' : ''}}">Cursos</a> </li>       
             <li> <a href="{{route('nosotros')}}" class="{{ request()->routeIs('nosotros') ? 'active' : ''}}">Nosotros</a> </li>
+            <li>
+                <a href="{{route('contactanos.index')}}" class="{{ request()->routeIs('contactanos.index') ? 'active' : ''}}">Contactanos</a>
+            </li>
 
         </ul>
     </nav>
