e99d66d58ba6c433cfff1134ff22a84a6bd805ca
E_n_i <nestorfm801@gmail.com>
2023-02-03 13:09:11 -0600

    Agregar solo la parte del logo y links (V.30.5)

--- Jetstream/resources/views/welcome.blade.php
+++ Jetstream/resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<x-applayout>
+
+</x-applayout>
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +134,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
