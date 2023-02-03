2185972b5051c7f483557d4ba3011e7a31f05051
E_n_i <nestorfm801@gmail.com>
2023-02-03 11:40:58 -0600

    Cambiando el texto que muestra (V.30)

--- Jetstream/resources/views/dashboard.blade.php
+++ Jetstream/resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Texto de ejemplo
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum provident alias, omnis dolores commodi maiores dolor, quam incidunt repellat totam amet, recusandae officia illum at perferendis deserunt vel nulla sequi?</p>
+                <p>Iure numquam vero nobis autem natus repudiandae deserunt soluta asperiores illum minus similique, veritatis beatae rerum consequuntur quas voluptatem, quae sint dignissimos voluptas delectus incidunt quibusdam eos? Cum, harum ratione.</p>
+                <p>Quaerat molestiae, non vitae voluptatem natus deleniti laborum suscipit, omnis optio, quos asperiores nulla provident. Ut sunt dolore explicabo, libero velit natus, commodi eum, dolorem cupiditate praesentium aliquid inventore iusto.</p>
+                <p>Nam, laborum eveniet! Iure numquam doloribus velit ut, commodi suscipit repudiandae accusantium, sint reiciendis necessitatibus culpa repellat accusamus laboriosam! Iusto architecto fugiat laborum nobis assumenda. Corporis, necessitatibus alias? Placeat, unde?</p>
+                <p>Accusantium ipsum recusandae, dicta itaque maxime quos expedita repudiandae non eaque magnam sit quibusdam molestias provident eligendi quas pariatur eum? Facere temporibus eaque illo ab inventore adipisci sed aspernatur illum!</p>
             </div>
         </div>
     </div>
